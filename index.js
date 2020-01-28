a11d4c2244181acc2a5d52edf093e3b0fd73f89a
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-01-28 12:32:13 -0800

    sixth test passsed

--- index.js
+++ index.js
@@ -188,8 +188,12 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let fullNames = [];
+  runners.forEach(function(item) {
+    return fullNames.push(`${item.last_name}, ${item.first_name}`);
+  });
+  return fullNames;
 }
 
 /**
